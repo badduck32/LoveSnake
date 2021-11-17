f1a9db16c149142b75730e702f984d073c58b615
Badduck <badduckssb@gmail.com>
2021-11-17 17:40:50 +0100

    Created a snake head and basic movement

--- main.lua
+++ main.lua
@@ -1,19 +1,43 @@
 function love.load()
-    test = false
+    dirHor = 1
+    dirVer = 0
+    headX = 100
+    headY = 100
+    moveSpeed = 25
+    cntr = moveSpeed
+    tail = {}
 end
 
 function love.update(dt)
-    -- body
+    cntr = cntr - 100 * dt
+    if cntr <= 0 then 
+        cntr = moveSpeed
+        headX = headX + 50 * dirHor
+        headY = headY + 50 * dirVer
+    end 
 end
 
 function love.draw()
-    if test then 
-        love.graphics.print("pressed", 10, 10)
-    end 
+    love.graphics.setColor(0, 0.8, 0)
+    love.graphics.rectangle("fill", headX, headY, 50, 50)
 end
 
 function love.keypressed(key)
     if key == "right" then
-        test = true 
+        dirHor = 1
+        dirVer = 0
+    elseif key == "left" then
+        dirHor = -1
+        dirVer = 0
+    elseif key == "up" then
+        dirVer = -1
+        dirHor = 0
+    elseif "down" then
+        dirVer = 1
+        dirHor = 0
     end
+end
+
+function moveSnake()
+
 end
