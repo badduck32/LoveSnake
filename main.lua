1783551e176c22b26e5b2bbd3239d0d78feae9b0
Badduck <badduckssb@gmail.com>
2021-11-17 22:55:07 +0100

    Added tail

--- main.lua
+++ main.lua
@@ -4,22 +4,26 @@ function love.load()
     headX = 100
     headY = 100
     moveSpeed = 25
+    snakeSize = 50
     cntr = moveSpeed
-    tail = {}
+    tailPosX = {}
+    tailPosY = {}
 end
 
 function love.update(dt)
     cntr = cntr - 100 * dt
     if cntr <= 0 then 
         cntr = moveSpeed
-        headX = headX + 50 * dirHor
-        headY = headY + 50 * dirVer
+        moveSnake()
     end 
 end
 
 function love.draw()
     love.graphics.setColor(0, 0.8, 0)
-    love.graphics.rectangle("fill", headX, headY, 50, 50)
+    love.graphics.rectangle("fill", headX, headY, snakeSize, snakeSize)
+    for i, v in pairs(tailPosX) do
+        love.graphics.rectangle("fill", tailPosX[i], tailPosY[i], snakeSize, snakeSize)
+    end
 end
 
 function love.keypressed(key)
@@ -32,12 +36,34 @@ function love.keypressed(key)
     elseif key == "up" then
         dirVer = -1
         dirHor = 0
-    elseif "down" then
+    elseif key == "down" then
         dirVer = 1
         dirHor = 0
+    else
+        growTail();
     end
 end
 
 function moveSnake()
+    for i, v in pairs(tailPosX) do
+        if #tailPosX - i > 0 then
+            tailPosX[#tailPosX - i + 1] = tailPosX[#tailPosX - i]
+            tailPosY[#tailPosY - i + 1] = tailPosY[#tailPosY - i]
+        else
+            tailPosX[1] = headX
+            tailPosY[1] = headY
+        end
+    end
+    headX = headX + snakeSize * dirHor
+    headY = headY + snakeSize * dirVer
+end
 
+function growTail()
+    if #tailPosX == 0 then
+        tailPosX[#tailPosX + 1] = headX
+        tailPosY[#tailPosY + 1] = headY
+    else
+        tailPosX[#tailPosX + 1] = tailPosX[#tailPosX]
+        tailPosY[#tailPosY + 1] = tailPosY[#tailPosY]
+    end
 end
